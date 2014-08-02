24ef3bf6072661e94a803bb368aae3a4424ec2c5
Adrienne Shen <pouringhoney@gmail.com>
2014-08-01 17:44:51 -0700

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HartlSampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
 
   match '/signup',  to: 'users#new',            via: 'get'
